04b99fccf7c6240068aaeb4a8bf71ac87a5420cf
Maxim Harizanov <maxim.harizanov@gmail.com>
2013-01-18 22:51:38 +0200

    Added a LocalInfo singleton class
    Contains the peer ID (for now)

---
+++ Torrent.Client/LocalInfo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Torrent.Client
+{
+    public sealed class LocalInfo
+    {
+        private const int ID_LENGTH = 20;
+        private const string ID_HEAD = "-RT1000-";
+
+        private static volatile LocalInfo instance = new LocalInfo();
+        private static object syncRoot = new object();
+
+        public byte[] PeerId { get; private set; }
+
+        public static LocalInfo Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        instance = new LocalInfo();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private LocalInfo()
+        {
+            PeerId = GeneratePeerId();
+        }
+
+        private byte[] GeneratePeerId()
+        {
+            var id = new List<byte>(ID_LENGTH);
+            var seed = DateTime.Now.Millisecond + DateTime.Now.Minute + DateTime.Now.Day + ID_HEAD.Length;
+            var random = new Random(seed);
+            id.AddRange(Encoding.UTF8.GetBytes(ID_HEAD));
+            id.AddRange(Enumerable.Repeat(0, ID_LENGTH - ID_HEAD.Length).Select(i => (byte)random.Next(128)));
+            return id.ToArray();
+        }
+
+        
+
+    }
+}
