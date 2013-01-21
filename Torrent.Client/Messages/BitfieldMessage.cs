30c570e2db8a5a1ea6fe64c00478bb0582701fcf
Dimitar Pankov <mitelinko@gmail.com>
2013-01-21 21:36:16 +0200

    Implemented BitfieldMessage.

--- Torrent.Client/Messages/BitfieldMessage.cs
+++ Torrent.Client/Messages/BitfieldMessage.cs
@@ -9,20 +9,56 @@ namespace Torrent.Client
     class BitfieldMessage:PeerMessage
     {
         public static readonly int Id = 5;
-
-        public override void FromBytes(byte[] buffer, int offset, int count)
+        public byte[] Bitfield { get; private set; }
+        
+        public BitfieldMessage()
         {
-            throw new NotImplementedException();
+            Bitfield = null;
+        }
+
+        public BitfieldMessage(byte[] bitfield)
+        {
+            this.Bitfield = bitfield;
         }
 
         public override int MessageLength
         {
-            get { throw new NotImplementedException(); }
+            get { return 4+1+Bitfield.Length; }
+        }
+
+        public override void FromBytes(byte[] buffer, int offset, int count)
+        {
+            if (count != MessageLength)
+                throw new ArgumentException("Invalid message length.");
+            this.Bitfield = ReadBytes(buffer, ref offset, count);
         }
 
         public override int ToBytes(byte[] buffer, int offset)
         {
-            throw new NotImplementedException();
+            int start = offset;
+            offset += Write(buffer, offset, (int)5);
+            offset += Write(buffer, offset, (byte)4);
+            offset += Write(buffer, offset, Bitfield);
+            return offset - start;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Have message: {Bitfield.Length: {0}}", Bitfield.Length);
+        }
+
+        public override bool Equals(object obj)
+        {
+            BitfieldMessage msg = obj as BitfieldMessage;
+
+            if (msg == null)
+                return false;
+            return CompareByteArray(this.Bitfield, msg.Bitfield);
+        }
+
+        public override int GetHashCode()
+        {
+            return MessageLength.GetHashCode() ^ Id.GetHashCode() ^ Bitfield.GetHashCode();
         }
     }
 }
