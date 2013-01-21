83fd21e36cafc503233e3386f9170b5cc7733f3c
Dimitar Pankov <mitelinko@gmail.com>
2013-01-21 22:54:47 +0200

    Implemented PortMessage.

--- Torrent.Client/Messages/PortMessage.cs
+++ Torrent.Client/Messages/PortMessage.cs
@@ -9,20 +9,56 @@ namespace Torrent.Client
     class PortMessage:PeerMessage
     {
         public static readonly int Id = 9;
+        public ushort Port { get; private set; }
+
+        public PortMessage()
+        {
+            this.Port = 0;
+        }
+
+        public PortMessage(ushort port)
+        {
+            this.Port = port;
+        }
 
         public override void FromBytes(byte[] buffer, int offset, int count)
         {
-            throw new NotImplementedException();
+            if (count != MessageLength)
+                throw new ArgumentException("Invalid message length.");
+            this.Port = (ushort)ReadShort(buffer, ref offset);
         }
 
         public override int MessageLength
         {
-            get { throw new NotImplementedException(); }
+            get { return 7; }
         }
 
         public override int ToBytes(byte[] buffer, int offset)
         {
-            throw new NotImplementedException();
+            int start = offset;
+            offset += Write(buffer, offset, (int)5);
+            offset += Write(buffer, offset, (byte)9);
+            offset += Write(buffer, offset, Port);
+            return offset - start;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Port message: {Port: {0}}", Port);
+        }
+
+        public override bool Equals(object obj)
+        {
+            PortMessage msg = obj as PortMessage;
+
+            if (msg == null)
+                return false;
+            return this.Port == msg.Port;
+        }
+
+        public override int GetHashCode()
+        {
+            return MessageLength.GetHashCode() ^ Id.GetHashCode() ^ Port.GetHashCode();
         }
     }
 }
