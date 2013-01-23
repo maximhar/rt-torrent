bf1599b648e5a0471ca81aa6d9607b640adf9cd1
Dimitar Pankov <mitelinko@gmail.com>
2013-01-24 01:30:12 +0200

    Added XML documentation to HaveMessage.

--- Torrent.Client/Messages/HaveMessage.cs
+++ Torrent.Client/Messages/HaveMessage.cs
@@ -1,38 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Torrent.Client
 {
+    /// <summary>
+    /// Provides a container class for the HaveMessage data for peer communication.
+    /// </summary>
     class HaveMessage:PeerMessage
     {
+        /// <summary>
+        /// The ID of the message
+        /// </summary>
         public static readonly int Id = 4;
+        /// <summary>
+        /// The zero-based index of a piece that has been successfully downloaded and verified via the hash.
+        /// </summary>
         public int PieceIndex { get; private set; }
 
+        /// <summary>
+        /// Initializes a new empty instance of the Torrent.Client.HaveMessage class.
+        /// </summary>
         public HaveMessage() 
         {
             PieceIndex = -1;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the Torrent.Client.HaveMessage class.
+        /// </summary>
+        /// <param name="pieceIndex">The zero-based index of a piece that has been successfully downloaded.</param>
         public HaveMessage(int pieceIndex)
         {
+            Contract.Requires(pieceIndex >= 0);
+
             this.PieceIndex = pieceIndex;
         }
 
+        /// <summary>
+        /// The lenght of the HaveMessage.
+        /// </summary>
         public override int MessageLength
         {
             get { return 9; }
         }
 
+        /// <summary>
+        /// Sets the HaveMessage properties via a byte array.
+        /// </summary>
+        /// <param name="buffer">The byte array containing the message data.</param>
+        /// <param name="offset">The position in the array at which the message begins.</param>
+        /// <param name="count">The length to be read in bytes.</param>
         public override void FromBytes(byte[] buffer, int offset, int count)
         {
             if (count != MessageLength)
                 throw new ArgumentException("Invalid message length.");
             this.PieceIndex = ReadInt(buffer, ref offset);
         }
-        
+
+        /// <summary>
+        /// Writes the HaveMessage data to a byte array.
+        /// </summary>
+        /// <param name="buffer">The byte array that the message data will be written to.</param>
+        /// <param name="offset">The position in the array at which the message begins.</param>
+        /// <returns>An integer representing the the amount of bytes written in the array.</returns>
         public override int ToBytes(byte[] buffer, int offset)
         {
             int start = offset;
@@ -42,11 +76,20 @@ namespace Torrent.Client
             return offset - start;
         }
 
+        /// <summary>
+        /// Returns a string that represents the content of the HaveMessage object.
+        /// </summary>
+        /// <returns>The string containing the HaveMessage data representation.</returns>
         public override string ToString()
         {
             return string.Format("Have message: {PieceIndex: {0}}", PieceIndex);
         }
 
+        /// <summary>
+        /// Determines wheteher this HaveMessage instance and a specified object, which also must be a HaveMessage object, have the same data values.
+        /// </summary>
+        /// <param name="obj">The HaveMessage to compare to this instance.</param>
+        /// <returns></returns>
         public override bool Equals(object obj)
         {
             HaveMessage msg = obj as HaveMessage;
@@ -56,6 +99,10 @@ namespace Torrent.Client
             return this.PieceIndex == msg.PieceIndex;
         }
 
+        /// <summary>
+        /// Returns the hash code for this HaveMessage instance.
+        /// </summary>
+        /// <returns>An integer representing the hash code of this instace of the HaveMessage class.</returns>
         public override int GetHashCode()
         {
             return MessageLength.GetHashCode() ^ Id.GetHashCode() ^ PieceIndex.GetHashCode();
