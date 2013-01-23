811f588dc841f319cce24b9fc1ba6051658d9665
Dimitar Pankov <mitelinko@gmail.com>
2013-01-24 01:49:28 +0200

    Added XML documentation to KeepAliveMessage.

--- Torrent.Client/Messages/KeepAliveMessage.cs
+++ Torrent.Client/Messages/KeepAliveMessage.cs
@@ -5,19 +5,40 @@ using System.Text;
 
 namespace Torrent.Client
 {
+    /// <summary>
+    /// Provides a container class for the KeepAliveMessage data for peer communication.
+    /// </summary>
     class KeepAliveMessage:PeerMessage
     {
-
+        /// <summary>
+        /// Initializes a new instance of the Torrent.Client.KeepAliveMessage class.
+        /// </summary>
         public KeepAliveMessage() { }
 
+        /// <summary>
+        /// The lenght of the KeepAliveMessage.
+        /// </summary>
         public override int MessageLength
         {
             get { return 1; }
         }
-        
+
+        /// <summary>
+        /// Sets the KeepAliveMessage properties via a byte array.
+        /// <para>This method has no use for the KeepAliveMessage class.</para>
+        /// </summary>
+        /// <param name="buffer">The byte array containing the message data.</param>
+        /// <param name="offset">The position in the array at which the message begins.</param>
+        /// <param name="count">The length to be read in bytes.</param>
         public override void FromBytes(byte[] buffer, int offset, int count)
         {  }
-        
+
+        /// <summary>
+        /// Writes the KeepAliveMessage data to a byte array.
+        /// </summary>
+        /// <param name="buffer">The byte array that the message data will be written to.</param>
+        /// <param name="offset">The position in the array at which the message begins.</param>
+        /// <returns>An integer representing the the amount of bytes written in the array.</returns>
         public override int ToBytes(byte[] buffer, int offset)
         {
             int start = offset;
@@ -25,16 +46,29 @@ namespace Torrent.Client
             return offset - start;
         }
 
+        /// <summary>
+        /// Returns a string that represents the content of the KeepAliveMessage object.
+        /// </summary>
+        /// <returns>The string containing the KeepAliveMessage data representation.</returns>
         public override string ToString()
         {
             return string.Format("KeepAlive message");
         }
 
+        /// <summary>
+        /// Determines wheteher this KeepAliveMessage instance and a specified object, which also must be a KeepAliveMessage object, have the same data values.
+        /// </summary>
+        /// <param name="obj">The KeepAliveMessage to compare to this instance.</param>
+        /// <returns></returns>
         public override bool Equals(object obj)
         {
             return obj is KeepAliveMessage;
         }
 
+        /// <summary>
+        /// Returns the hash code for this KeepAliveMessage instance.
+        /// </summary>
+        /// <returns>An integer representing the hash code of this instace of the KeepAliveMessage class.</returns>
         public override int GetHashCode()
         {
             return MessageLength.GetHashCode();
