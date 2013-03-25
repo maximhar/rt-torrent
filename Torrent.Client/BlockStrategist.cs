384d38406213c031d42553770c675a055571682d
Maxim Harizanov <maxim.harizanov@gmail.com>
2013-03-25 21:34:48 +0200

    Added more code to ch. 3

--- Torrent.Client/BlockStrategist.cs
+++ Torrent.Client/BlockStrategist.cs
@@ -58,23 +58,22 @@ namespace Torrent.Client
             BlockInfo block;
             int counter = 0;
             do
-            {
+            {   //увеличаваме брояча на опитите за намиране на произволен блок
                 counter++;
                 int index;
                 lock(unavailable)
-                {
+                {   //ако има липсващи блокове, избираме случаен от тях
                     if (unavailable.Any())
                         index = unavailable.Random();
-                    else return BlockInfo.Empty;
+                    else return BlockInfo.Empty; //в противен случай, връщаме празен
                 }
-                
+                //преобразуване на адрес
                 block = Block.FromAbsoluteAddress((long)index*blockSize, pieceSize, blockSize,
                                                  totalSize);
-                if (counter > 10)
+                if (counter > 10) //ако броячът за опитите надвиши 10, връщаме блока, независимо дали пиъра го има
                     return block;
+                //повторяме докато не установим, че bitfield-а на пиъра съдържа произволно избрания блок
             } while (!bitfield[block.Index]);
-
-            Debug.WriteLine("Strategist requested block " + block.Index);
             return block;
         }
 
@@ -82,14 +81,15 @@ namespace Torrent.Client
         {   //изчисляване на адреса на блока
             int address = (int)(Block.GetAbsoluteAddress(block.Index, block.Offset, pieceSize)/blockSize);
             lock (unavailable)
-            {
+            {   //ако колекцията с липсващи блокове включва адреса на блока, и блока е с дължина
+                //по-голяма от 0, тогава го обработваме
                 if(unavailable.Contains(address) && block.Length > 0)
-                {
+                {   //премахване на новия блок от колекцията със липсващи
                     Debug.WriteLine("Needed block incoming:" + address);
                     unavailable.Remove(address);
-                    Available++;
-                    pieces[block.Index] -= block.Length;
-                    if(pieces[block.Index]<=0)
+                    Available++; //увеличаване на броя на наличните блокове
+                    pieces[block.Index] -= block.Length; //изваждане на размера на блока от размера на парчето, което го съдържа
+                    if(pieces[block.Index]<=0) //ако парчето, съдържащо блока, има размер 0, тогава обявяваме парчето за свалено
                     {
                         SetDownloaded(block.Index);
                     }
